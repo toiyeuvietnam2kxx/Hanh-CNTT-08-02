a087dc28b314a27d5d1df12e5e75369ef84aac2e
toiyeuvietnam2kxx <toiyeuvietnam2kxx@gmail.com>
2015-04-03 10:19:19 +0700

    HoangHanhCNTT08-02 baitap 26.cpp

---
+++ HoangHanhCNTT08-02 baitap 26.cpp
@@ -0,0 +1,25 @@
+﻿// CHƯƠNG TRÌNH TÍNH S= 1/2 + 2/3 + 3/4 +…+ N/ (N+1)
+#include<iostream>
+using namespace std;
+void main()
+{
+
+	int i, n;
+
+	float S = 0;
+
+	cout << "Nhap n="; cin >> n; cin.ignore();
+
+	for (i = 1; i <= n; i++)
+
+	{
+
+		S = S + (float)i / (i + 1);
+
+	}
+
+	cout << " Vay s =  " << S;
+	cin.get();
+
+
+}
